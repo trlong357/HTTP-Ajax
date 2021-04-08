c5c0a58e353006e7c1fdd09fc4f3fae7c640b2f6
Long Tran <trlong357@gmail.com>
2021-04-08 14:15:48 +0700

    194. Rendering Components for Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import axios from "axios";
-// import Posts from "./Posts/Posts";
+import Posts from "./Posts/Posts";
 import { Route } from "react-router-dom";
 import "./Blog.css";
 
@@ -20,8 +20,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <h1>Home</h1>} />
-        <Route path="/new-post" exact render={() => <h1>New Post</h1>} />
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/new-post" exact render={() => <h1>New Post</h1>} /> */}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
